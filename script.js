f15fc608cb247207a9f66eda68d6b70421769aa7
pjha91275 <pjha91275@gmail.com>
2025-07-08 01:10:07 +0530

    completed 60% project

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const addTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('task-list');
     const emptyImage = document.querySelector('.empty-image');
+    const todosContainer = document.querySelector('.todos-container');
 
     const toggleEmptyState = ()=>{
         emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
-    }
+        todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
+    };
     
     const addTask = (event)=>{
         event.preventDefault();
@@ -17,7 +19,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         const li = document.createElement('li');
         li.innerHTML = `<input type = "checkbox" class = "checkbox">
-        <span>${taskText}</span>`;
+        <span>${taskText}</span>
+        <div class="task-buttons">
+           <button class = "edit-btn"><i class = "fa-solid fa-pen"></I></button>
+           <button class = "delete-btn"><i class = "fa-solid fa-trash"></I></button>
+        </div>`;
+
+        const checkbox = li.querySelector('.checkbox');
+        const editBtn = li.quqerySelector('.edit-btn');
+
+        editBtn.addEventListener('click', ()=>{
+            if(!checkbox.checked){
+                taskInput.value = li.querySelector('span').textContent;
+                 li.remove;
+                 toogleEmptyState();
+            }
+        });
+
+        li.querySelector('.delete-btn').addEventListener('click', ()=>{
+            li.remove;
+            toogleEmptyState();
+        });
+
         taskList.appendChild(li);
         taskInput.value = '';
         toggleEmptyState();
