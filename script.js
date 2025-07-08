de620aaf80d57309b0a1b5fa979f84c4d0848ea4
pjha91275 <pjha91275@gmail.com>
2025-07-08 07:51:48 +0530

    Completed 70%

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
     };
     
-    const addTask = (event)=>{
+    const addTask = (text,completed = false)=>{
         event.preventDefault();
         const taskText = taskInput.value.trim();
         if(!taskText){
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         const li = document.createElement('li');
-        li.innerHTML = `<input type = "checkbox" class = "checkbox">
+        li.innerHTML = `<input type = "checkbox" class = "checkbox" ${completed ? 'checked' : ''} />
         <span>${taskText}</span>
         <div class="task-buttons">
            <button class = "edit-btn"><i class = "fa-solid fa-pen"></I></button>
@@ -27,6 +27,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         const checkbox = li.querySelector('.checkbox');
         const editBtn = li.quqerySelector('.edit-btn');
+
+
+        if(completed){
+            li.classList.add('completed');
+            editBtn.disabled = true;
+            editBtn.style.opacity = '0.5';
+            editBtn.style.pointerEvents = 'none';
+        }
+
+        checkbox.addEventListener('change',()=>{
+            const isChecked = checkbox.checked;
+            li.classList.toggle('completed',isChecked);
+            editBtn.disabled = isChecked;
+            editBtn.style.opacity = isChecked ? '0.5' : '1';
+            editBtn.style.pointerEvents = isChecked ? 'none':'auto';
+        })
 
         editBtn.addEventListener('click', ()=>{
             if(!checkbox.checked){
